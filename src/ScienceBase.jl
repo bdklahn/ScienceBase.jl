11a5482421cb5d4feb5baeb202d879de83e43780
Brian D. Klahn <briandk@virginia.edu>
2025-04-03 13:27:11 -0400

    now able to pull data from the API

--- src/ScienceBase.jl
+++ src/ScienceBase.jl
@@ -1,5 +1,40 @@
 module ScienceBase
 
+using URIs
+using HTTP
+using JSON3
+
 include("constants.jl")
 
+"""
+Constructs a URI for accessing ScienceBase API.
+"""
+function make_uri(
+    domain::AbstractString=domain, endpoint::Endpoint=catalog, objecttype::ObjectType=item;
+    objectid::AbstractString=ScienceBase_root_item_ID,
+    query::AbstractString="format=json",
+    kwargs...,
+)
+    endpoint, objecttype = string(endpoint), string(objecttype)
+
+    path = joinpath("/", endpoint, objecttype, objectid)
+
+    if !isempty(kwargs)
+        query *= "&$(escapeuri(kwargs))"
+    end
+    URI(;scheme="https", host=domain, path, query)
 end
+
+function get_data(uri::URI=make_uri(), outdir::AbstractString="data", cacheintermediates::Bool=true,)
+    resp = HTTP.get(uri)
+    content_type = get(Dict(resp.headers), "Content-Type", nothing)
+    if occursin("application/json", content_type)
+        # JSON response
+        data = JSON3.read(resp.body)
+        return data
+    end
+    resp.data
+end
+
+
+end # module ScienceBase
--- src/constants.jl
+++ src/constants.jl
@@ -1,2 +1,27 @@
 "Main domain"
 const domain = "www.sciencebase.gov"
+
+@enum Endpoint catalog
+
+"main endpoint for ScienceBase data catalog"
+catalog
+
+@enum ObjectType item file
+
+"The main container for ScienceBase data. These can point to files, have parents, children, and metadata."
+item
+
+"A file type object. This can be a child of an item. This can be a file in the ScienceBase catalog or an external link."
+file
+
+"sciencebase.gov USGS ScienceBase-Catalog ROOT item ID"
+const ScienceBase_root_item_ID = "4f4e4760e4b07f02db47df9b"
+
+"ScienceBase Catalog item ID"
+const ScienceBaseCatalog_item_ID = "4f4e4760e4b07f02db47df9c"
+
+"National Minerals Information Center item ID"
+const NMIC_item_ID = "5c8c03e4e4b0938824529f7d"
+
+"Item ID for Mineral Commodity Summaries 2025 Data Release"
+const MSC_2025_item_ID = "677eaf95d34e760b392c4970"
